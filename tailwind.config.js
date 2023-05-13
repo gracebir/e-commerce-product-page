d325175a6327059747e2e677c2c24f79b8fb1b2b
gracebir <grace.devolop@gmail.com>
2023-05-13 20:36:05 +0200

    setting up the project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "orange-color":"hsl(26, 100%, 55%)",
+        "pale-orange":"hsl(25, 100%, 94%)",
+        "very-dark-blue":"hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue":"hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        "white-color":"hsl(0, 0%, 100%)",
+        "black-opac":"hsl(0, 0%, 0%)"
+      },
+      fontFamily: ['Kumbh Sans', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
