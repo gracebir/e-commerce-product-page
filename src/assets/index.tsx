e553c85e156437b123d1375bceaed24f48f573fb
gracebir <grace.devolop@gmail.com>
2023-05-13 21:37:16 +0200

    import and exporting assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg'
+import menu from './icon-menu.svg'
+import close from './icon-close.svg'
+import basket from './icon-cart.svg'
+import plus from './icon-plus.svg'
+import next from './icon-next.svg'
+import previous from './icon-previous.svg'
+import avatar from './image-avatar.png'
+
+
+export {
+    logo,
+    menu,
+    close,
+    basket,
+    plus,
+    next,
+    previous,
+    avatar
+}
