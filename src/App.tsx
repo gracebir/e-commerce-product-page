099d679a889d69dc8d0fa25b2e27bc12bc0bbcc3
gracebir <grace.devolop@gmail.com>
2023-05-13 21:38:00 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <div className="bg-light-grayish-blue">
       <div className="max-w-7xl mx-auto px-4">
-        
+        <Navbar/>
       </div>
     </div>
   )
